d7346c50f54ca3e3fba69434ccb0ef788d01d923
win7_test <a260641139@ya.ru>
2018-02-20 16:38:40 +0300

    dfghdf

--- WebHookBot.py
+++ WebHookBot.py
@@ -6,20 +6,19 @@ import logging
 
 API_TOKEN = '548697540:AAHbO142Q-p98C0-uvqpvi2-eYnSd4RdWno'
 
-WEBHOOK_HOST = '7f95526c.ngrok.io'
-WEBHOOK_PORT = 443  # 443, 80, 88 or 8443 (port need to be 'open')
+WEBHOOK_HOST = 'fc823ddf.ngrok.io'
+WEBHOOK_PORT = 80  # 443, 80, 88 or 8443 (port need to be 'open')
 WEBHOOK_LISTEN = '0.0.0.0'  # In some VPS you may need to put here the IP addr
 
-WEBHOOK_SSL_CERT = 'C:\Users\win7_test\PycharmProjects\WebHookBot\webhook_cert.pem'  # Path to the ssl certificate
-WEBHOOK_SSL_PRIV = 'C:\Users\win7_test\PycharmProjects\WebHookBot\webhook_pkey.pem'  # Path to the ssl private key
+WEBHOOK_SSL_CERT = 'C:\Users\win7_test\PycharmProjects\WebHookBot\YOURPUBLIC.pem'  # Path to the ssl certificate
+WEBHOOK_SSL_PRIV = 'C:\Users\win7_test\PycharmProjects\WebHookBot\YOURPRIVATE.key'  # Path to the ssl private key
 
 # Quick'n'dirty SSL certificate generation:
 #
-# openssl genrsa -out webhook_pkey.pem 2048
-# openssl req -new -x509 -days 3650 -key webhook_pkey.pem -out webhook_cert.pem
+#openssl req -newkey rsa:2048 -sha256 -nodes -keyout YOURPRIVATE.key -x509 -days 365 -out YOURPUBLIC.pem -subj "/C=US/ST=New York/L=Brooklyn/O=Example Brooklyn Company/CN=fc823ddf.ngrok.io"
 #
-# When asked for "Common Name (e.g. server FQDN or YOUR name)" you should reply
-# with the same value in you put in WEBHOOK_HOST
+#curl -F "url=https://fc823ddf.ngrok.io/548697540:AAHbO142Q-p98C0-uvqpvi2-eYnSd4RdWno/" -F "certificate=YOURPUBLIC.pem" "https://api.telegram.org/bot548697540:AAHbO142Q-p98C0-uvqpvi2-eYnSd4RdWno/setwebhook"
+#
 
 WEBHOOK_URL_BASE = "https://%s" % (WEBHOOK_HOST)
 WEBHOOK_URL_PATH = "/%s/" % (API_TOKEN)
@@ -36,14 +35,13 @@ app = flask.Flask(__name__)
 # Empty webserver index, return nothing, just http 200
 @app.route('/', methods=['GET', 'HEAD'])
 def index():
-    print "OKay"
+    print bot.get_webhook_info()
     return ''
 
 
 # Process webhook calls
 @app.route(WEBHOOK_URL_PATH, methods=['POST'])
 def webhook():
-    print "HOOK"
     if flask.request.headers.get('content-type') == 'application/json':
         json_string = flask.request.get_data().decode('utf-8')
         update = telebot.types.Update.de_json(json_string)
@@ -68,15 +66,15 @@ def echo_message(message):
 
 
 # Remove webhook, it fails sometimes the set if there is a previous webhook
-bot.remove_webhook()
+#bot.remove_webhook()
 
 # Set webhook
-bot.set_webhook(url=WEBHOOK_URL_BASE+WEBHOOK_URL_PATH, certificate=open(WEBHOOK_SSL_CERT, 'r'))
+#bot.set_webhook(url=WEBHOOK_URL_BASE+WEBHOOK_URL_PATH, certificate=open(WEBHOOK_SSL_CERT, 'r'))
 
 # Start flask server
 app.run(host=WEBHOOK_LISTEN,
         port=WEBHOOK_PORT,
-        ssl_context=(WEBHOOK_SSL_CERT, WEBHOOK_SSL_PRIV),
+        #ssl_context=(WEBHOOK_SSL_CERT, WEBHOOK_SSL_PRIV),
         debug=True)
 
 if __name__ == "__main__":
--- sender.py
+++ sender.py
@@ -1,11 +1,15 @@
 import requests
 
-cert_file_path = "C:\Users\win7_test\PycharmProjects\WebHookBot\webhook_cert.pem"
-key_file_path = "C:\Users\win7_test\PycharmProjects\WebHookBot\webhook_pkey.pem"
+cert_file_path = "C:\Users\win7_test\PycharmProjects\WebHookBot\YOURPUBLIC.pem"
+key_file_path = "C:\Users\win7_test\PycharmProjects\WebHookBot\YOURPRIVATE.key"
 
 cert = (cert_file_path, key_file_path)
 
-url = "https://7f95526c.ngrok.io/548697540:AAHbO142Q-p98C0-uvqpvi2-eYnSd4RdWno/"
+#url = "https://fc823ddf.ngrok.io/548697540:AAHbO142Q-p98C0-uvqpvi2-eYnSd4RdWno/"
+#data = {"URL": "865d62ad.ngrok.io"}
+#req = requests.post(url, json=data, cert=cert, verify=True)
+
+url = "https://fc823ddf.ngrok.io/"
 data = {"URL": "865d62ad.ngrok.io"}
-req = requests.post(url, json=data, cert=cert, verify=False)
+req = requests.get(url, cert=cert, verify=False)
 print req.status_code
