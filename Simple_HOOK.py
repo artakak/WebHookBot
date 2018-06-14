8e52dcac0c2b5022d4bedb38dfea24173df6c9fa
artakak <a260641139@ya.ru>
2018-06-14 13:03:32 +0300

    sdfds

--- Simple_HOOK.py
+++ Simple_HOOK.py
@@ -7,12 +7,15 @@ import os
 
 dir_path = os.path.dirname(os.path.realpath(__file__))
 
+# юда нам надо запихнуть токен нашего телеграмм бота
 API_TOKEN = 'Bot_token'
 
+# сюда указываем параметры нашего хоста, на котором будет все это крутиться, должен быть виден снаружи для серверов телеграмма
 WEBHOOK_HOST = 'HOSTNAME'
 WEBHOOK_PORT = 8443  # 443, 80, 88 or 8443 (port need to be 'open')
 WEBHOOK_LISTEN = '127.0.0.1'  # In some VPS you may need to put here the IP addr
 
+# это пути к сертификатам, которые мы должны сгенерить командами ниже, команды для консоли
 WEBHOOK_SSL_CERT = "%s/YOURPUBLIC.pem" % dir_path  # Path to the ssl certificate
 WEBHOOK_SSL_PRIV = "%s/YOURPRIVATE.key" % dir_path # Path to the ssl private key
 
@@ -35,14 +38,14 @@ bot = telebot.TeleBot(API_TOKEN)
 app = flask.Flask(__name__)
 
 
-# Empty webserver index, return nothing, just http 200
+# Роут для индексной страницы, при запросе возвращает 200
 @app.route('/', methods=['GET', 'HEAD'])
 def index():
     # Index page
     return ''
 
 
-# Process webhook calls
+# Роут для вебхука телеграмма, сюда будут стучаться сервера телеграмма, когда что-то упадет в нашего бота от пользователей
 @app.route(WEBHOOK_URL_PATH, methods=['POST'])
 def webhook():
     if flask.request.headers.get('content-type') == 'application/json':
@@ -54,7 +57,7 @@ def webhook():
         flask.abort(403)
 
 
-# Handle '/start' and '/help'
+# Обработака команд '/start' and '/help'
 @bot.message_handler(commands=['help', 'start'])
 def send_welcome(message):
     bot.reply_to(message,
@@ -62,23 +65,24 @@ def send_welcome(message):
                   "I am here to echo your kind words back to you."))
 
 
-# Handle '/end'
+# Обработка команды '/end'
 @bot.message_handler(commands=['send'])
 def send_letter(message):
     bot.reply_to(message, "Ваше сообщение обрабатывается")
 
 
-# Handle all other messages
+# Обработка остальных сообщений
 @bot.message_handler(func=lambda message: True, content_types=['text'])
 def echo_message(message):
     bot.reply_to(message, message.text)
 
 
-# Remove webhook, it fails sometimes the set if there is a previous webhook
+# Обнуляет вебхук с сертификатами
 #bot.remove_webhook()
 
-# Set webhook
+# Установка вебхука и сертификатов, запускаем один раз
 #bot.set_webhook(url=WEBHOOK_URL_BASE+WEBHOOK_URL_PATH, certificate=open(WEBHOOK_SSL_CERT, 'r'))
+
 
 if __name__ == '__main__':
     app.run(host=WEBHOOK_LISTEN, port=WEBHOOK_PORT, debug=True)
